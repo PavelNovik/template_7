7aa7ca8e5a55a097bbd69ae6f8bbbc7ff4027479
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-08-06 01:16:56 +0300

    Finish home section

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,2 +1,23 @@
 console.log('hello from index.js');
 console.log('Starting project!');
+
+const swiper = new Swiper('.swiper', {
+  cssMode: true,
+  loop: true,
+
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    renderBullet: function (index, className) {
+      return (
+        '<span class="' + className + '" style="margin: 2rem">' + '</span>'
+      );
+    },
+  },
+  mousewheel: true,
+  keyboard: true,
+});
