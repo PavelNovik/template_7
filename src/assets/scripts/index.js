668eb56205424b2f1eb1c4bce418f74131812495
PavelNovik <92117738+PavelNovik@users.noreply.github.com>
2023-08-08 00:38:46 +0300

    add cards in services & about sections

--- src/assets/scripts/index.js
+++ src/assets/scripts/index.js
@@ -1,10 +1,17 @@
-console.log('hello from index.js');
-console.log('Starting project!');
-
+// Initialize Home swiper-slider with swiperJS API
 const swiper = new Swiper('.swiper', {
+  speed: 600,
+  parallax: true,
   cssMode: true,
   loop: true,
 
+  keyboard: {
+    enabled: true,
+  },
+  autoplay: {
+    delay: 4500,
+    disableOnInteraction: false,
+  },
   navigation: {
     nextEl: '.swiper-button-next',
     prevEl: '.swiper-button-prev',
@@ -21,3 +28,29 @@ const swiper = new Swiper('.swiper', {
   mousewheel: true,
   keyboard: true,
 });
+
+// Initialize sections (parts)
+const sections = document.querySelectorAll('section');
+
+// Initialize navigation buttons and scroll function
+const navigationBtns = document.querySelectorAll('.list_item');
+
+navigationBtns.forEach((btn) => {
+  btn.addEventListener('click', function (event) {
+    let coordY;
+    sections.forEach((section) => {
+      coordY =
+        section.id === event.target.textContent ? section.offsetTop : coordY;
+    });
+    window.scroll({
+      top: coordY,
+      left: 0,
+      behavior: 'smooth',
+    });
+  });
+});
+
+// Initialize custom view sections (opacity duration) with scrollReveal API
+sections.forEach((section) =>
+  ScrollReveal().reveal(`#${section.id}`, { duration: 700 })
+);
